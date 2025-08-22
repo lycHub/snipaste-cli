03df3ecb82446c9ec220656a6a20e05325e247fa
liyichao <yichao.li@zeiss.com>
2025-08-22 17:56:15 +0800

    ss

--- packages/playground/i18n.config.js
+++ packages/playground/i18n.config.js
@@ -5,18 +5,4 @@ export default {
   // mainLang: Langs[0],
   resourcePath: "",
   tranDest: "i18n",
-
-  tencent: {
-    credential: {
-      secretId: "AKID1vcByhR0e5IGzVny72jFXaOxIOEpBIAn",
-      secretKey: "QindEdpnX3LQGrENJ1VlnZ6PYT9a0FOV",
-    },
-    region: "ap-shanghai",
-    profile: {
-      httpProfile: {
-        endpoint: "tmt.tencentcloudapi.com",
-      },
-    },
-    projectId: 0,
-  },
 };
